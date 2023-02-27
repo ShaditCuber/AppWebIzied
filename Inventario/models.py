a2c31bac0bcd9c097b251cf4622e03ed13fb6fb2
Felipe Bastidas <64512945+ShaditCuber@users.noreply.github.com>
2023-02-27 17:15:50 -0300

    Modificaciones

--- Inventario/models.py
+++ Inventario/models.py
@@ -1,14 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+
+class warehouse(models.Model):
+    idWarehouse=models.CharField(max_length=100,null=False,blank=False,primary_key=True)
+    name=models.CharField(max_length=100,null=False,blank=False)
+
 class Inventory(models.Model):
+    code=models.CharField(max_length=100,null=False,blank=False,primary_key=True)
     name = models.CharField(max_length=100,null=False,blank=False)
-    price=models.IntegerField(null=False,blank=False)
-    stock=models.IntegerField(null=False,blank=False)
+    priceUnit=models.IntegerField(null=False,blank=False)
+    resume=models.CharField(max_length=500,null=False,blank=False)
+    # stock=models.IntegerField(null=False,blank=False)
     # typeUnit=models.CharField(max_length=100,null=False,blank=False)
     creationDate=models.DateField(auto_now_add=True)
     updateDate=models.DateField(auto_now=True)
-    
+    idWarehouse=models.ForeignKey(warehouse,blank=True,null=False,on_delete=models.CASCADE)
     
     def __str__(self) -> str:
         return self.name
+    
+class Inventario(models.Model):
+    name = models.CharField(max_length=100,null=False,blank=False)
+    stock=models.IntegerField(null=False,blank=False)
