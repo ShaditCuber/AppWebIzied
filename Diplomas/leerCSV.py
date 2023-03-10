549ec89db8ca43e4dcf5a7a8731cabf10ebe88d1
Felipe Bastidas <64512945+ShaditCuber@users.noreply.github.com>
2023-03-10 01:29:45 -0300

    Todo

--- Diplomas/leerCSV.py
+++ Diplomas/leerCSV.py
@@ -231,11 +231,13 @@ def leer(csvFile,encuestasFile,codigo):
         partesP=1
     dfPartsPorcentaje = np.array_split(table, int(partesP))
     for i, part in enumerate(dfPartsPorcentaje):
-        fill_colors = ['lavender' if i%2==0 else 'white' for i in range(len(table))]  # Color de fondo para filas pares e impares
+          # Color de fondo para filas pares e impares
         
         if i == len(dfPartsPorcentaje) - 1:
             part.loc[len(part)] = ['Total', total_alumnos, '100%']
-            fill_colors[-1] = 'paleturquoise'  # Cambiar color de fondo de la última fila
+            
+        fill_colors = ['lavender' if i%2==0 else 'white' for i in range(len(table))]
+        fill_colors[-1] = 'paleturquoise'  # Cambiar color de fondo de la última fila
         fig = go.Figure(data=[
                                 go.Table(
                                     header=dict(values=list(part.columns),align='center',fill_color='#6ec63b',font=dict(color='white',family='Open Sans')),
