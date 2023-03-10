4e255df7366fa6fd35846576f835571e79a2f62c
Felipe Bastidas <64512945+ShaditCuber@users.noreply.github.com>
2023-03-10 13:34:32 -0300

    Todo

--- Diplomas/encuestaOCR.py
+++ Diplomas/encuestaOCR.py
@@ -266,7 +266,7 @@ def ocr(ruta):
     # Crear la figura y agregar la tabla
     fig = go.Figure(go.Table(
         header=dict(values=["Pregunta", "Promedio"],align='center',fill_color='#6ec63b',font=dict(color='white')),
-        cells=dict(values=[preguntas, divididos]),
+        cells=dict(values=[preguntas, divididos],style={'whiteSpace': 'normal', 'height': 'auto'}),
     ),layout=layout)
     name='./tmp/tablaEncuesta.png'
     fig.update_layout(title='Tabla Promedio por Pregunta',title_x=.5)
